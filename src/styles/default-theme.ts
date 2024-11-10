c2b49ac179956e8fdeab3dc5215c7268022a3ebd
Pablo Alan <pabloxt14@gmail.com>
2024-11-10 16:27:34 -0300

    style: add app token styles

---
+++ src/styles/default-theme.ts
@@ -0,0 +1,37 @@
+export const defaultTheme = {
+  colors: {
+    gray: {
+      50: '#fafafa',
+      100: '#Eff0f0',
+      200: '#dddedf',
+      300: '#b9bbbc',
+      600: '#5c6265',
+      900: '#333638',
+      950: '#1b1d1e',
+    },
+
+    green: {
+      100: '#e5f0db',
+      200: '#cbe4b4',
+      500: '#639339',
+    },
+
+    red: {
+      100: '#F4E6E7',
+      300: '#F3BABD',
+      600: '#BF3B44',
+    },
+  },
+  fontFamily: {
+    regular: 'NunitoSans_400Regular',
+    bold: 'NunitoSans_700Bold',
+  },
+  fontSize: {
+    xs: 12,
+    sm: 14,
+    base: 16,
+    lg: 18,
+    xl: 24,
+    '2xl': 32,
+  },
+}
