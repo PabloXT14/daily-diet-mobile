07e6042aa13c1aad27fe0b4994c8295226f1033b
Pablo Alan <pabloxt14@gmail.com>
2024-11-10 16:57:52 -0300

    style: add phosphor-icons lib

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ import {
   NunitoSans_700Bold,
 } from '@expo-google-fonts/nunito-sans'
 import { defaultTheme } from '@/styles/default-theme'
+import { Rocket } from 'phosphor-react-native'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -22,6 +23,7 @@ export default function App() {
     <ThemeProvider theme={defaultTheme}>
       <View style={styles.container}>
         <Text>Open up App.tsx to start working on your app!</Text>
+        <Rocket size={24} />
         <StatusBar style="dark" translucent />
       </View>
     </ThemeProvider>
