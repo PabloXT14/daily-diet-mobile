a5d91e2b266eb09f2ec322ab109f31a936846f07
Pablo Alan <pabloxt14@gmail.com>
2024-11-13 21:29:45 -0300

    feat: (home screen) create meal-item component

---
+++ src/components/meal-item/index.tsx
@@ -0,0 +1,21 @@
+import type { MealDTO } from '@/@types/meal'
+
+import { Container, Divider, Status, Time, Title } from './styles'
+
+type MealItemProps = {
+  meal: MealDTO
+}
+
+export function MealItem({ meal }: MealItemProps) {
+  return (
+    <Container>
+      <Time>{new Date(meal.datetime).toLocaleTimeString('pt-BR')}</Time>
+
+      <Divider />
+
+      <Title>{meal.name}</Title>
+
+      <Status variant={meal.isInDiet ? 'primary' : 'secondary'} />
+    </Container>
+  )
+}
---
+++ src/components/meal-item/styles.ts
@@ -0,0 +1,53 @@
+import { css, styled } from 'styled-components/native'
+
+export const Container = styled.View`
+  flex-direction: row;
+  align-items: center;
+  gap: 12px;
+
+  padding: 14px 12px 14px 16px;
+
+  border-radius: 6px;
+  border: 1px solid ${({ theme }) => theme.colors.gray[200]};
+`
+
+export const Time = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.colors.gray[950]};
+    font-family: ${theme.fontFamily.regular};
+    font-size: ${theme.fontSize.xs}px;
+    line-height: 16px; /* 130% */
+  `}
+`
+
+export const Divider = styled.View`
+  width: 1px;
+  height: 100%;
+
+  background-color: ${({ theme }) => theme.colors.gray[300]};
+`
+
+export const Title = styled.Text`
+  flex: 1;
+
+  ${({ theme }) => css`
+    color: ${theme.colors.gray[900]};
+    font-family: ${theme.fontFamily.regular};
+    font-size: ${theme.fontSize.base}px;
+    line-height: 20px; /* 130% */
+  `}
+`
+
+type StatusProps = {
+  variant: 'primary' | 'secondary'
+}
+
+export const Status = styled.View<StatusProps>`
+  width: 14px;
+  height: 14px;
+
+  border-radius: 9999px;
+
+  background-color: ${({ theme, variant }) =>
+    variant === 'primary' ? theme.colors.green[200] : theme.colors.red[300]};
+`
