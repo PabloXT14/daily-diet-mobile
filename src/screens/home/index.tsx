339dd70df7c236ad839cf99fc858103a4f438582
Pablo Alan <pabloxt14@gmail.com>
2024-11-15 16:51:26 -0300

    feat: create gradient

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,6 +1,7 @@
 import { FlatList } from 'react-native'
 import { useTheme } from 'styled-components/native'
 import { Plus } from 'phosphor-react-native'
+import { LinearGradient } from 'expo-linear-gradient'
 
 import { Profile } from '@/components/profile'
 import { Button } from '@/components/button'
@@ -123,6 +124,17 @@ export function Home() {
           }}
         />
       </MealsContainer>
+
+      <LinearGradient
+        colors={['transparent', colors.gray[50]]}
+        style={{
+          position: 'absolute',
+          bottom: 0,
+          left: 0,
+          right: 0,
+          height: 160,
+        }}
+      />
     </Container>
   )
 }
